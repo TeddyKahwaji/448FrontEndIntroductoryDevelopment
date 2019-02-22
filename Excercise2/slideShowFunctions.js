a4db97e0e19f92ee04b536a9c5e12e4274ae79ac
Teddy <teddykahwaji86@gmail.com>
2019-02-22 17:05:56 -0600

    Buttons Code from JS implemented

--- Excercise2/slideShowFunctions.js
+++ Excercise2/slideShowFunctions.js
@@ -1,8 +1,12 @@
 let arr = ["91YLuOr4bmL._UY606_.jpg","jwgibbo.jpg","1200px-McDonald's_Golden_Arches.svg.jpg", "6cf2bd7eecb487aa57b8e63dc449bfc4.jpg"
           , "0messy-desk.jpg"];
-let next = 1;
+let next = 0;
+
+
+
 imageCollector = function()
 {
+    next++;
   if(next ==5)
   {
     next =0;
@@ -10,11 +14,25 @@ imageCollector = function()
   let img1 = document.getElementById("image1");
 
   img1.src = arr[next];
-  next++;
+
+
 
 }
 
-imageBacker = function()
+backwards = function()
 {
-  
+  next--;
+
+
+
+  if(next ==-1)
+  {
+    next =4;
+
+  }
+  let img1 = document.getElementById("image1");
+    img1.src = arr[next];
+
+
+
 }
